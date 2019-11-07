427c95a181d129d8e6fb83fc2f8b2f5485c2c611
lutingzc <2519822113@qq.com>
2019-11-07 20:15:40 +0800

    各种验证

--- src/main/java/com/lutingting/StringUtils.java
+++ src/main/java/com/lutingting/StringUtils.java
@@ -1,4 +1,9 @@
 package com.lutingting;
+
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 
  * @ClassName: 判断一个字符串是否为空 
@@ -11,5 +16,51 @@ public class StringUtils {
 	public static boolean isBlank(String str) {
 		return (str==null||str.trim().equals(""));
 	}
+	/**
+	 * @Title: haveValue 
+	 * @Description: 判断是否有值
+	 * @param str
+	 * @return
+	 * @return: boolean
+	 */
+	public static boolean haveValue(String str) {
+		return !(str==null||str.trim().equals(""));
+	}
+	/**
+	 * 
+	 * @Title: isMobile 
+	 * @Description: 验证手机号码
+	 * @return
+	 * @return: boolean
+	 */
+	public static boolean isMobile(String str) {
+		String regex="^([0-9]+\\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\\.[0-9]+)|([0-9]*[1-9][0-9]*)$";
+		Pattern p=Pattern.compile(regex);
+		Matcher matcher=p.matcher(str);
+		return matcher.find();
+	}
+	/**
+	 * 
+	 * @Title: isEmail 
+	 * @Description: 验证邮件
+	 * @param str
+	 * @return
+	 * @return: boolean
+	 */
+	public static boolean isEmail(String str) {
+		String regex="^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
+		Pattern p=Pattern.compile(regex);
+		Matcher matcher=p.matcher(str);
+		return matcher.find();
+	}
 	
+	public static String getRandomStr(int n) {
+		StringBuilder sb=new StringBuilder();
+		Random random=new Random();
+		for (int i = 0; i < n; i++) {
+			sb.append((char)'A'+ random.nextInt(26));
+		}
+		
+		return sb.toString();
+	}
 }
