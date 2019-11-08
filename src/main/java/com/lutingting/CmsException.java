cebda9425dff1729d5c9d318a4fa47d5fa819fed
lutingzc <2519822113@qq.com>
2019-11-08 13:44:28 +0800

    验证

---
+++ src/main/java/com/lutingting/CmsException.java
@@ -0,0 +1,10 @@
+package com.lutingting;
+
+public class CmsException extends Exception {
+
+	public CmsException(String string) {
+		super(string);
+	}
+
+	private static final long serialVersionUID=-2388326871380439088L;
+}
